f1e813d3858605e2b0a3f9cad6ccc0fedf3a60b5
raymac2020 <ray.mcelhinney@gmail.com>
2012-11-17 09:34:35 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hardwarestore::Application.config.secret_token = '97649f088cd313fc3a88923f83761038302a6db35d7d68f447b1263b03d339d41b8f0212c19bc2b6d6682359465c5c1a656af94849dcb3537818cf63df750a45'
